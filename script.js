f105dc4bd6f77b94f657d107f617c268abd5cc2a
Sebastián Perouene Muanna <116591919+Choba07@users.noreply.github.com>
2023-04-12 07:56:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-const form = document.querySelector('#contact-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const message = document.querySelector('#message').value;
+ document.querySelector('#contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    swal({
+      title: "Mensaje enviado",
+      text: "Gracias por contactarnos, nos pondremos en contacto contigo a la brevedad",
+      icon: "success",
+    });
+    document.querySelector('#contact-form').reset();
+  });
+
   
-  // Guardar la información en una base de datos o en una variable
-  // Por ejemplo, aquí lo estamos mostrando en la consola
-  console.log(`Nombre: ${name}\nEmail: ${email}\nMensaje: ${message}`);
-
-  // Mostrar una alerta de que la información se envió correctamente
-  alert('¡Gracias por enviar tu información!');
-});
-
 
 
