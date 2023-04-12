3b4e2c8f53dd5e7bf1a1465f47675e95815985df
Seba.git <sebastianperouene@gmail.com>
2023-04-12 18:20:34 -0300

    mensaje de commit

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ document.querySelector('#contact-form').addEventListener('submit', function(even
   });
   document.querySelector('#contact-form').reset();
 });
+
+fetch('productos.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Imprime los datos del archivo JSON en la consola
+    
+    const buyButtons = document.querySelectorAll('.buy-button'); // Selecciona todos los botones de compra
+    buyButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const productName = button.parentElement.querySelector('h3').innerText; // Obtiene el nombre del producto del elemento h3 dentro del elemento padre del botón
+        Swal.fire({
+          title: `Has añadido ${productName} a tu carrito de compras`,
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        });
+      });
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
